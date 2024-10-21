c396e4c844d8625caff84b82a180c6cc7d5d289c
VBV1 <104235290+VBV11@users.noreply.github.com>
2024-10-21 21:51:43 +0200

    Create WPG.ps1

---
+++ WPG.ps1
@@ -0,0 +1,26 @@
+$O = "$env:USERPROFILE\wifi-wachtwoorden.txt"
+$P = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+if (Test-Path $O) { Clear-Content $O }
+
+foreach ($I in $P) {
+    $R = netsh wlan show profile name="$I" key=clear
+    $S = $I
+    $A = ($R | Select-String "Authentication" | ForEach-Object { $_.ToString().Split(":")[1].Trim() })
+    $C = ($R | Select-String "Cipher" | ForEach-Object { $_.ToString().Split(":")[1].Trim() })
+    $K = $R | Select-String "Key Content" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+
+    Add-Content $O "`nSSID: $S"
+    Add-Content $O "Authenticatie: $A"
+    Add-Content $O "Encryptie: $C"
+    
+    if ($K) {
+        Add-Content $O "Wachtwoord: $K`n"
+    } else {
+        Add-Content $O "Wachtwoord: Niet beschikbaar`n"
+    }
+}
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+} else {
+    Invoke-Item $O
+}
